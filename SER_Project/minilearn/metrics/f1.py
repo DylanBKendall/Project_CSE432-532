f03c82f74be53ede0faa87e5bd74af10f281382e
kendaldb <kendaldb@miamioh.edu>
2026-05-05 11:01:55 -0400

    Fixed f1_score

--- SER_Project/minilearn/metrics/f1.py
+++ SER_Project/minilearn/metrics/f1.py
@@ -3,8 +3,8 @@ import accuracy
 
 def f1_score(y_true, y_pred):
 
-    precision_score = precision_score(y_true, y_pred)
-    accuracy_score = accuracy_score(y_true, y_pred)
+    precision_score = precision.precision_score(y_true, y_pred)
+    accuracy_score = accuracy.accuracy_score(y_true, y_pred)
 
     # returns the harmonic mean of precision and accuracy scores
     return 2 / ((1.0 / precision_score) + (1.0 / accuracy_score))
